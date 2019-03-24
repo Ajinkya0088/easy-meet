04737b4260507dc3bee3ed1296ff224cc35b126e
10aditya <adityapingle90@gmail.com>
2019-03-24 15:19:38 +0530

    sign up and login completed

---
+++ EasyMeet/app/src/main/java/com/nerds/easymeet/Constants.java
@@ -0,0 +1,4 @@
+package com.nerds.easymeet;
+
+public class Constants {
+}
---
+++ EasyMeet/app/src/main/java/com/nerds/easymeet/LauncherActivity.java
@@ -0,0 +1,87 @@
+package com.nerds.easymeet;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        changeFragment(new WelcomeFragment());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (currentUser != null) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            changeFragment(new MeetingsFragment());
+                        }
+                    });
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showNoAccountFoundAlert();
+                        }
+                    });
+                }
+            }
+        }, 2000);
+
+
+    }
+
+    private void showNoAccountFoundAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.no_account_alert_layout, null);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        view.findViewById(R.id.sign_up_cv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                changeFragment(new SignUpFragment());
+            }
+        });
+        view.findViewById(R.id.log_in_cv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                changeFragment(new LoginFragment());
+            }
+        });
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+        fragmentTransaction.replace(R.id.fragment, fragment);
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+}
