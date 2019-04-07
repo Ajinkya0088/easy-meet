5dc56ac1d3a4ab4ba24ffc99a8c4ff23c0915b62
10aditya <adityapingle90@gmail.com>
2019-04-07 19:52:52 +0530

    added firebase to dashboard

--- web/easy-meet-web/src/environments/environment.prod.ts
+++ web/easy-meet-web/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCkTzQrYLUqAp5Dwi_li2XPUJBa8VvTkc0",
+    authDomain: "easymeet-21e7c.firebaseapp.com",
+    databaseURL: "https://easymeet-21e7c.firebaseio.com",
+    projectId: "easymeet-21e7c",
+    storageBucket: "easymeet-21e7c.appspot.com",
+    messagingSenderId: "520745758148"
+  }
 };
--- web/easy-meet-web/src/environments/environment.ts
+++ web/easy-meet-web/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCkTzQrYLUqAp5Dwi_li2XPUJBa8VvTkc0",
+    authDomain: "easymeet-21e7c.firebaseapp.com",
+    databaseURL: "https://easymeet-21e7c.firebaseio.com",
+    projectId: "easymeet-21e7c",
+    storageBucket: "easymeet-21e7c.appspot.com",
+    messagingSenderId: "520745758148"
+  }
 };
 
 /*
